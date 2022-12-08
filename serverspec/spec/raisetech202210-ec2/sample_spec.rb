44f357168b7da1b0274d2acbbd7814acc2ad7e33
MasatoshiMizumoto <51846213+MasatoshiMizumoto@users.noreply.github.com>
2022-12-08 22:07:54 +0900

    change healthcheck

--- serverspec/spec/raisetech202210-ec2/sample_spec.rb
+++ serverspec/spec/raisetech202210-ec2/sample_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-listen_port = 8080
+listen_port = 80
 
 describe package('nginx') do
   it { should be_installed }
@@ -10,6 +10,6 @@ describe port(listen_port) do
   it { should be_listening }
 end
 
-describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+describe command('curl http://127.0.0.1:#{listen_port}/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
